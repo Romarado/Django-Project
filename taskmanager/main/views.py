8980e2a7e5bcf93a44d08f8dac454f7d84352352
Romarado <pombl4.xd@gmail.com>
2021-02-16 23:52:09 +0300

    removed unnecessary code

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -16,8 +16,6 @@ class CustomSuccessMessageMixin:
     @property
     def success_msg(self):
         return False
-    def test(self):
-        pass
     def form_valid(self, form):
         messages.success(self.request, self.success_msg)
         return super().form_valid(form)
